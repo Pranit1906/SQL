e897d921278e791339d12a658ffc81df5d6fe135
Pranit <pranitdubal5@gmail.com>
2023-06-14 01:27:07 +0530

    sql

---
+++ mySqlBasic/mySqlBasic.js
@@ -0,0 +1,71 @@
+/*
+
+SQL (Structured Query language)
+SQL is a standard language for storing, manipulating and retrieving data in databases
+
+
+SQL is a standard language for accessing and manipulating databases.
+
+What is SQL?
+SQL stands for Structured Query Language
+SQL lets you access and manipulate databases
+SQL became a standard of the American National Standards Institute (ANSI) in 1986,
+ and of the International Organization for Standardization (ISO) in 1987
+
+
+What Can SQL do?
+SQL can execute queries against a database
+SQL can retrieve data from a database
+SQL can insert records in a database
+SQL can update records in a database
+SQL can delete records from a database
+SQL can create new databases
+SQL can create new tables in a database
+SQL can create stored procedures in a database
+SQL can create views in a database
+SQL can set permissions on tables, procedures, and views
+
+
+*/
+
+/*
+
+RDBMS
+RDBMS stands for Relational Database Management System.
+
+RDBMS is the basis for SQL, and for all modern database systems such as MS SQL Server,
+ IBM DB2, Oracle, MySQL, and Microsoft Access.
+
+The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and 
+it consists of columns and rows.
+
+Every table is broken up into smaller entities called fields. The fields in the Customers table consist of 
+CustomerID, CustomerName, ContactName, Address, City, PostalCode and Country. A field is a column in a table
+that is designed to maintain specific information about every record in the table.
+
+A record, also called a row, is each individual entry that exists in a table. For example, there are 91 records
+ in the above Customers table. A record is a horizontal entity in a table.
+
+A column is a vertical entity in a table that contains all information associated with a specific field in a table.
+
+*/
+
+/*
+
+-------------------------------------------Some of The Most Important SQL Commands-------------------------------------------
+
+SELECT - extracts data from a database
+UPDATE - updates data in a database
+DELETE - deletes data from a database
+INSERT INTO - inserts new data into a database
+CREATE DATABASE - creates a new database
+ALTER DATABASE - modifies a database
+CREATE TABLE - creates a new table
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index
+
+
+*/
+
